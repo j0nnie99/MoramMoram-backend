e00930384dcfa13971c9887e87ba97929c49359b
Hyun Woo <hwsa10041@gmail.com>
2022-11-17 23:25:03 +0900

    [FEAT] 플리마켓 엔티티 구현 및 data.sql 업데이트

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,11 @@ INSERT INTO authority (authority_name) VALUES ('ROLE_USER');
 INSERT INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 INSERT INTO authority (authority_name) VALUES ('ROLE_OFFICE');
 
-INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,maketing) VALUES (1,'1000playch@naver.com','admin','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-1234-1234',null,true,0,true);
-INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,maketing) VALUES (2,'6candoit@naver.com','user','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-1234-1234',null,true,0,true);
-INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,maketing,office_add,maket_add) VALUES (3,'6candoit@gmail.com','office','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-5678-5678',null,true,0,true,"dk.jpn","aa.png");
+INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,marketing) VALUES (1,'1000playch@naver.com','admin','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-1234-1234',null,true,0,true);
+INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,marketing) VALUES (2,'6candoit@naver.com','user','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-1234-1234',null,true,0,true);
+INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,marketing,office_add,market_add) VALUES (3,'6candoit@gmail.com','office','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-5678-5678',null,true,0,true,"dk.jpg","aa.png");
+
+INSERT INTO flea_market (id, category, end, m_img, m_name, m_note, office_id, open, place, start) VALUES (1,'의류/잡화','2022-12-23','market.png','한강 플리마켓','환영합니다',3,false,'영등포','2022-12-25');
 
 INSERT INTO user_authority (id,authority_name) VALUES (1,'ROLE_ADMIN');
 INSERT INTO user_authority (id,authority_name) VALUES (1,'ROLE_USER');
