ad75775c98fb76e5265907ef30d475a1bc9b6125
Hyun Woo <hwsa10041@gmail.com>
2022-11-17 13:39:32 +0900

    #14 회원탈퇴 기능을 위한 상태코드 구현

--- src/main/java/kusitms/candoit/MoramMoramServer/global/Model/Status.java
+++ src/main/java/kusitms/candoit/MoramMoramServer/global/Model/Status.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 public enum Status {
     SOCIAL_ADD_INFO_PLZ("추가 정보 요청"),
     SOCIAL_ADD_INFO_STAUTS_TRUE("추가 정보 요청 성공"),
+    USER_DELETE_STATUS_TRUE("회원탈퇴 성공"),
     LOGOUT_TRUE("로그아웃 성공");
     private final String statusMessage;
 }
