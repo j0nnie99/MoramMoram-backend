094c43f21315f8f6bf1ed7c570d92659263eec2b
Hyun Woo <hwsa10041@gmail.com>
2022-11-18 16:03:18 +0900

    [FEAT] 찜 엔티티 개발 / 찜 기능 구현 / 추가된 data.sql 공유

--- src/main/java/kusitms/candoit/MoramMoramServer/global/Model/Status.java
+++ src/main/java/kusitms/candoit/MoramMoramServer/global/Model/Status.java
@@ -13,6 +13,8 @@ public enum Status {
     SOCIAL_ADD_INFO_STAUTS_TRUE("추가 정보 요청 성공"),
     USER_DELETE_STATUS_TRUE("회원탈퇴 성공"),
     PROFILE_IMAGE_UPLOAD_TRUE("이미지 업로드 성공"),
+    LICENSE_UPLOAD_TRUE("사업자 등록증 업로드 성공"),
+    FLEAMARKET_LIKE_TRUE("플리마켓 찜하기 성공"),
     LOGOUT_TRUE("로그아웃 성공");
     private final String statusMessage;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,17 @@ INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,marketing) VALUES (1,
 INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,marketing) VALUES (2,'6candoit@naver.com','user','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-1234-1234',null,true,0,true);
 INSERT INTO user (id,email,name,pw,pnum,uimg,seller,report,marketing,office_add,market_add) VALUES (3,'6candoit@gmail.com','office','$2a$10$D99zvd9eSCquwrkA5ss7L.GiYshRHu2x.MEgvTbk80SpnGahGNKse','010-5678-5678',null,true,0,true,"dk.jpg","aa.png");
 
-INSERT INTO flea_market (id, category, end, m_img, m_name, m_note, office_id, open, place, start) VALUES (1,'의류/잡화','2022-12-23','market.png','한강 플리마켓','환영합니다',3,false,'영등포','2022-12-25');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (1,'의류/잡화','2022-11-23','market.png','한강 플리마켓','환영합니다',3,false,'영등포','2022-11-25','2022-11-21');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (2,'먹거리','2022-12-01','market.png','구롱구롱 플리마켓','환영합니다',3,false,'구로','2022-12-05','2022-11-28');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (3,'인테리어','2022-12-21','market.png','도리미 플리마켓','환영합니다',3,false,'신도림','2022-12-25','2022-12-17');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (4,'문구/팬시','2022-12-23','market.png','잠실만 플리마켓','환영합니다',3,false,'잠실','2022-12-28','2022-12-20');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (5,'수공예품','2022-11-13','market.png','강자만 남는 플리마켓','환영합니다',3,false,'강남','2022-11-16','2022-11-10');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (6,'의류/잡화','2022-11-23','market.png','거꾸로해도 플리마켓','환영합니다',3,false,'역삼','2022-11-25','2022-11-20');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (7,'의류/잡화','2022-11-20','market.png','홍시대박 플리마켓','환영합니다',3,false,'홍대','2022-11-23','2022-11-17');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (8,'인테리어','2022-11-19','market.png','어청담 플리마켓','환영합니다',3,false,'청담','2022-11-27','2022-11-16');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (9,'수공예품','2022-12-02','market.png','어디연남 플리마켓','환영합니다',3,false,'연남','2022-12-05','2022-11-30');
+INSERT INTO flea_market (id, category, start, m_img, m_name, m_note, office_id, open, place, end, deadline) VALUES (10,'먹거리','2022-12-04','market.png','마음이 공덕공덕 플리마켓','환영합니다',3,false,'공덕','2022-12-08','2022-12-01');
+
 
 INSERT INTO user_authority (id,authority_name) VALUES (1,'ROLE_ADMIN');
 INSERT INTO user_authority (id,authority_name) VALUES (1,'ROLE_USER');
