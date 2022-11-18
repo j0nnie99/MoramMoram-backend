0bc11f6f1e45b654aa36e3feabc2c5563fc3ffe9
Hyun Woo <hwsa10041@gmail.com>
2022-11-18 18:04:24 +0900

    [FIX] Test중 오류 과정 제거 branch_master

--- src/test/java/kusitms/candoit/MoramMoramServer/service/BoardServiceTests.java
+++
@@ -1,9 +0,0 @@
-package kusitms.candoit.MoramMoramServer.service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class BoardServiceTests {
-
-}
