0769d04728ac04d67c8a046b6a4fff9878388b78
yoonjeongin <yoonjeongin@naver.com>
2022-11-19 03:38:50 +0900

    [FEAT] 신청서 생성/조회/수정/삭제 구현
    - 이미지 처리 필요

---
+++ src/main/java/kusitms/candoit/MoramMoramServer/domain/application/Controller/ApplicationController.java
@@ -0,0 +1,53 @@
+package kusitms.candoit.MoramMoramServer.domain.application.Controller;
+
+import kusitms.candoit.MoramMoramServer.domain.application.Dto.ApplicationDto;
+import kusitms.candoit.MoramMoramServer.domain.application.Entity.Application;
+import kusitms.candoit.MoramMoramServer.domain.application.Service.ApplicationService;
+import kusitms.candoit.MoramMoramServer.global.config.Response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/my-applications")
+public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    // 신청서 작성하기
+    @PostMapping("/new")
+    public BaseResponse<?> newApplication(@RequestBody ApplicationDto reqBody){
+        Application applicationEntity = applicationService.newApplication(reqBody);
+        Long appId = applicationEntity.getApplicationId();
+        return new BaseResponse<>(appId);
+    }
+
+    // 신청서 수정하기
+    @PatchMapping("/edit?app_id={applicationId}")
+    public BaseResponse<?> editApplication(@RequestBody ApplicationDto reqBody, @PathVariable Long applicationId){
+        applicationService.editApplication(reqBody, applicationId);
+        return new BaseResponse<>(applicationId);
+    }
+
+    // 신청서 삭제하기
+    @DeleteMapping("/delete?app_id={applicationId}")
+    public BaseResponse<?> deleteApplication(@PathVariable Long applicationId){
+        applicationService.deleteApplication(applicationId);
+        return new BaseResponse<>(applicationId);
+    }
+
+    // 내 신청서 모아보기
+    @GetMapping("/my-applications")
+    public ResponseEntity<?> myApplications(){
+        List<Application> apps = applicationService.getMyApplications();
+        return new ResponseEntity<>(apps, HttpStatus.OK);
+    }
+
+    // 신청서 상세보기
+
+
+}
